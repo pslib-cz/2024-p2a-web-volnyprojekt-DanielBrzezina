6f0e210c9e33cf285e9289ae63ff582d7f31c41b
DanielBrzezina <daniel.brzezina.023@pslib.cz>
2025-05-20 22:08:49 +0200

    gallerye

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,16 @@ import './swiper.js'
 
 
 
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+import myGallery from 'lightgallery';
+
+// Plugins
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
+myGallery(document.querySelector(".gallery"), {
+  plugins: [lgZoom, lgThumbnail],
+  selector: '.picturelink', thumbnail: true,
+
+});
